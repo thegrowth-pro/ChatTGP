400c716ade2ffde2b1e753f435832f20dae7eef9
Vicente Agüero <vicentea@thegrowth.pro>
2025-07-29 15:14:37 -0400

    feat(streamlit): :sparkles: Add init files

---
+++ app/components/__init__.py
@@ -0,0 +1,10 @@
+# app/components/__init__.py
+
+from .admin_automation import admin_automation
+from .client_editor import client_editor
+from .api_response import api_response
+from .client_list import client_list
+from .prompt_tree import prompt_tree
+from .session_box import session_box
+from .auth_form import auth_form
+from .api_form import api_form
