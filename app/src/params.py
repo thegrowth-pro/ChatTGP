f8a77e9f1c99b185e82f7a21c6a25ecefeec208f
Vicente Agüero <vicentea@thegrowth.pro>
2025-07-29 15:19:20 -0400

    feat(streamlit): :sparkles: Add params file

---
+++ app/src/params.py
@@ -0,0 +1,48 @@
+# app/src/params.py
+
+import os
+
+APP_NAME = 'app'
+
+# Root Folders
+DATA_FOLDER = 'data'
+ASSETS_FOLDER = 'assets'
+
+# App Folders
+PAGES_FOLDER = 'sites'
+
+# App Logos
+LOGO_FILE = 'logo.jpg'
+FAVICON_FILE = 'logo.jpg'
+
+# About Info
+README_ABOUT = 'ABOUT.md'
+GITHUB_URL = 'https://github.com/thegrowth-pro/ChatTGP'
+REPORT_URL_MAIL = 'mailto:vicentea@thegrowth.pro'
+
+################################################################################
+
+# Paths
+BASE_PATH = os.path.join(os.path.dirname(__file__), '..', '..')
+DATA_PATH = os.path.join(BASE_PATH, DATA_FOLDER)
+ASSETS_PATH = os.path.join(BASE_PATH, ASSETS_FOLDER)
+
+# App Path
+APP_PATH = os.path.join(BASE_PATH, APP_NAME)
+
+# Paths
+PATHS = {
+    'logo': os.path.join(ASSETS_PATH, LOGO_FILE),
+    'favicon': os.path.join(ASSETS_PATH, FAVICON_FILE),
+    'readme_about': os.path.join(APP_PATH, README_ABOUT),
+    'pages': {
+        os.path.splitext(x)[0]: os.path.join(APP_PATH, PAGES_FOLDER, x)
+        for x in os.listdir(os.path.join(APP_PATH, PAGES_FOLDER))
+    }
+}
+
+################################################################################
+
+# About Text
+with open(PATHS['readme_about']) as f:
+    ABOUT_TEXT = f.read()
